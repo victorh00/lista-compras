d15e327fca891b6c7bee4dac5553264dcfb0e5e7
monicahillman <mmhillman1@ucs.br>
2024-11-14 10:22:20 -0300

    feat: aula 01

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputItem = document.getElementById("input-item")
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    console.log(inputItem.value);
+})
