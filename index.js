401073a7868e009e2060f7edc3833ec206f07d59
Monica Hillman <51282495+MonicaHillman@users.noreply.github.com>
2024-11-24 19:11:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const inputItem = document.getElementById("input-item")
 
-const botaoAdicionar = document.getElementById("adicionar-item");
-
-botaoAdicionar.addEventListener("click", (evento) => {
-    evento.preventDefault();
-    console.log(inputItem.value);
-})
+console.log(inputItem.value);
